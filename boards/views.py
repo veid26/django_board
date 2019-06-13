8a75096edd7748b16193d36a4cc9f05174eb36e6
veid26 <veid_26@mail.ru>
2019-06-13 10:37:07 +0300

    some

--- boards/views.py
+++ boards/views.py
@@ -15,7 +15,7 @@ class BoardListView(ListView):
 	context_object_name = 'boards'
 	template_name = 'boards.html'
 
-class TopicListView(ListView):
+"""class TopicListView(ListView):
     model = Topic
     context_object_name = 'topics'
     template_name = 'topics.html'
@@ -28,9 +28,9 @@ class TopicListView(ListView):
     def get_queryset(self):
         self.board = get_object_or_404(Board, pk=self.kwargs.get('pk'))
         queryset = self.board.topics.order_by('-last_updated').annotate(replies=Count('posts') - 1)
-        return queryset
+        return queryset"""
 	
-class PostListView(ListView):
+"""class PostListView(ListView):
     model = Post
     context_object_name = 'posts'
     template_name = 'topic_posts.html'
@@ -48,7 +48,7 @@ class PostListView(ListView):
     def get_queryset(self):
         self.topic = get_object_or_404(Topic, board__pk=self.kwargs.get('pk'), pk=self.kwargs.get('topic_pk'))
         queryset = self.topic.posts.order_by('created_at')
-        return queryset
+        return queryset"""
 
 def home(request):
 	all_boards = Board.objects.all()
@@ -56,59 +56,3 @@ def home(request):
 	posts_count = Post.objects.all().count()
 	return render(request, 'home.html',
 	context ={'all_boards':all_boards, 'boards_count':boards_count})
-
-
-def new_topic(request, pk):
-    board = get_object_or_404(Board, pk=pk)
-    if request.method == 'POST':
-        form = NewTopicForm(request.POST)
-        if form.is_valid():
-            topic.board = board
-            topic.save()
-            Post.objects.create(
-                message=form.cleaned_data.get('message'),
-                topic=topic,
-            )
-            return redirect('topic_posts', pk=pk, topic_pk=topic.pk)
-    else:
-        form = NewTopicForm()
-    return render(request, 'new_topic.html', {'board': board, })
-
-def reply_topic(request, pk, topic_pk):
-    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.topic = topic
-            post.save()
-
-            topic.last_updated = timezone.now()
-            topic.save()
-
-            topic_url = reverse('topic_posts', kwargs={'pk': pk, 'topic_pk': topic_pk})
-            topic_post_url = '{url}?page={page}#{id}'.format(
-                url=topic_url,
-                id=post.pk,
-                page=topic.get_page_count()
-            )
-
-            return redirect(topic_post_url)
-    else:
-        form = PostForm()
-    return render(request, 'reply_topic.html', {'topic': topic})
-
-
-class PostUpdateView(UpdateView):
-    model = Post
-    fields = ('message', )
-    template_name = 'edit_post.html'
-    pk_url_kwarg = 'post_pk'
-    context_object_name = 'post'
-
-    def form_valid(self, form):
-        post = form.save(commit=False)
-        post.updated_by = self.request.user
-        post.updated_at = timezone.now()
-        post.save()
-        return redirect('topic_posts', pk=post.topic.board.pk, topic_pk=post.topic.pk)
